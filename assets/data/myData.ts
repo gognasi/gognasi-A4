a597020ff65b6de0f928c59458435021c5c4c94d
gognasi <52754625+gognasi@users.noreply.github.com>
2019-08-09 11:25:50 -0400

    Add files via upload

---
+++ assets/data/myData.ts
@@ -0,0 +1,8 @@
+import { Gognasi } from '../../app/gognasi'
+
+export const GOGNASI: Gognasi[] = 
+[{studentNumber: 991439731, name: 'Simranpreet Gogna',loginName: 
+'gognasi', campus: 'davis', assignmentTitle: 'Assignment 4 Angular More'}]
+
+
+
